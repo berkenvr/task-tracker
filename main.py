77a1730d50dd238ed60d042975ce2ad95bf2751d
Berke <berkeonver@icloud.com>
2025-02-22 13:03:37 +0300

    add, list commands added

--- main.py
+++ main.py
@@ -1,23 +1,74 @@
 import json
 import os.path
+from datetime import datetime
 
+def parse_command(command):
+    command = command.split()
 
-def load_database(path):
-    try:
-        with open(path) as f:
-            database = json.load(f)
-    except FileNotFoundError:
-        database = {}
-    return database
+    c_code = command[:1]
+    params = ' '.join(command[1:])
 
-def save_database(database, path):
-    with open(path, "w") as f:
-        json.dump(database, f)
+    return run_command(c_code[0], params)
+
+def run_command(c_code, params):
+    if c_code == 'add':
+        return add_task(params)
+    elif c_code == 'list':
+        return list_task()
+    else:
+        return f'Unknown command'
+
+def add_task(params):
+    current_time = datetime.now()
+    f_time = current_time.strftime('%H:%M %d %B %Y')
+
+    if not os.path.exists('tasks.json'):
+        w_json = [
+            {
+            'id': 1,
+            'description': params,
+            'status': 'todo',
+            'createdAt': f_time,
+            'updatedAt': f_time
+            }
+        ]
+        with open('tasks.json', 'w') as f:
+            json.dump(w_json, f, indent = 4)
+    else:
+        with open('tasks.json', 'r') as f:
+            data = json.load(f)
+            data.append(
+                {
+                    'id': data[-1]['id'] + 1,
+                    'description': params,
+                    'status': 'todo',
+                    'createdAt': f_time,
+                    'updatedAt': f_time
+                }
+            )
+        with open('tasks.json', 'w') as f:
+            json.dump(data, f, indent = 4)
+
+    return f'Task added'
+
+def list_task():
+    if not os.path.exists('tasks.json'):
+        return f'There is no file to list.'
+    else:
+        with open('tasks.json', 'r') as f:
+            data = json.load(f)
+            for i in range(0, len(data)):
+                print(data[i])
+    return f'All tasks listed'
 
 def main():
-    DATABASE_PATH = os.path.expanduser("~/tasks.json")
-    database = load_database(DATABASE_PATH)
-    save_database(database, DATABASE_PATH)
+    try:
+        while True:
+            command = input('task-cli ')
+            result = parse_command(command)
+            print(result)
+    except KeyboardInterrupt:
+        print('Tasking completed')
 
 if __name__ == '__main__':
     main()
